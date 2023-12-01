b7d9e124611de36517adc6754f49c52298d8283b
Maverick Vitto <mavvitto@yahoo.com>
2023-11-30 21:27:00 -0700

    phase 1 tasklister mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+  })
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', handleFormSubmit);
+});
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  const taskInput = document.getElementById('new-task-description');
+  const taskDescription = taskInput.value;
+  taskInput.value = ''; 
+  const taskList = document.getElementById('tasks');
+  const newTask = document.createElement('li');
+  newTask.textContent = taskDescription;
+  taskList.appendChild(newTask)
+}
